5d5b7570fa3147a7e19eb34abb1a5f1630520749
Gabriel Goldas <gabriel.goldas@gmail.com>
2025-02-11 10:24:28 -0300

    add navigation with React Router

---
+++ src/pages/items/ListItems.jsx
@@ -0,0 +1,5 @@
+export default function Listitems() {
+  return (
+    <h2>Todos os itens</h2>
+  )
+}
---
+++ src/pages/items/UpdateItem.jsx
@@ -0,0 +1,5 @@
+export default function UpdateItem() {
+  return (
+    <h2>Atualizar Item</h2>
+  )
+}
